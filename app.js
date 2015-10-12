94d991e89dfa8539f0946e4a7591f876ae367bed
zEdmundo <edmundo-rodrigues@hotmail.com.br>
2015-10-11 21:51:30 -0300

    added fahrenheit to #1

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ app.controller('TemperaturaController', function() {
   this.celsius = 32.0;
 
   this.celsiusToKelvin = function(celsius) {
-    answer = parseInt(celsius) + 273.5;
+    answer = parseFloat(celsius) + 273.5;
     if (isNaN(answer)) {
       return 0;
     } else {
       return  answer;
     }
   };
+
+  this.celsiusToFahrenheit = function (celsius) {
+    var answer = 32 + (9*parseFloat(celsius))/5;
+
+    if (isNaN(answer))
+      return 0;
+
+    return answer;
+  }
 
 });
